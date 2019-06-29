b2e90ac669f77ecc5fa2efe948e20da3fce8888d
charpell <charpellumeh@gmail.com>
2019-06-29 10:09:35 +0100

    Browse

--- config/fbConfig.js
+++ config/fbConfig.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDTkHV7QXyp8i7Csq-oIYci9iNOSbyEOq0",
-  authDomain: "net-ninjs-marioplan-101.firebaseapp.com",
-  databaseURL: "https://net-ninjs-marioplan-101.firebaseio.com",
-  projectId: "net-ninjs-marioplan-101",
-  storageBucket: "",
-  messagingSenderId: "386100492720",
-  appId: "1:386100492720:web:11cbd686d4f38ebe"
+  apiKey: "AIzaSyAdhAPQxetEk6T_LcLFGvCE3QSffFOdvBo",
+    authDomain: "reactbootcamp101.firebaseapp.com",
+    databaseURL: "https://reactbootcamp101.firebaseio.com",
+    projectId: "reactbootcamp101",
+    storageBucket: "reactbootcamp101.appspot.com",
+    messagingSenderId: "765260015341",
+    appId: "1:765260015341:web:08c8831632da9025"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- screens/Browse.js
+++ screens/Browse.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react'
+import { Dimensions, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 
-import { Block, Button, Text } from '../components'
+import { Block, Button, Text, Card, Badge } from '../components'
+import { theme, mocks } from '../constants';
 import { signOut } from '../store/actions/authActions'
+
+const { width } = Dimensions.get('window');
+
 
 import firebase from '../config/fbConfig';
 
 class Browse extends Component {
   static navigationOptions = {
-    title: "Browse",
     header: null
   }
-
-  constructor(props){
-    super(props);
-    this.state = {
-      name: "",
-      email: ""
-    }
-  }
-
-
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(authenticate => {
-     console.log('authenticate', authenticate)
-    })
+  state = {
+    name: "",
+    email: "",
+    active: 'Products',
+    categories: []
   }
 
   componentDidMount() {
@@ -38,6 +33,21 @@ class Browse extends Component {
         this.props.navigation.replace("Login")
       }
     })
+    this.setState({ categories: this.props.categories })
+    this.props.categories.forEach(function(obj) {
+      firebase.firestore().collection('categories').add({
+        id: obj.id,
+        name: obj.name,
+        tags: obj.tags,
+        count: obj.count,
+        image: obj.image
+      }).then(function(docRef) {
+        console.log('Document written with ID: ', docRef.id)
+      })
+      .catch(function(error) {
+        console.log('Error adding document: ', error)
+      })
+    })
   }
 
 
@@ -48,20 +58,88 @@ class Browse extends Component {
     })
   }
 
+  handleTab = tab => {
+    const { categories } = this.props;
+    console.log('categories', categories)
+    const filtered = categories.filter(
+      category => category.tags.includes(tab.toLowerCase())
+    );
+
+    this.setState({ active: tab, categories: filtered });
+  }
+
+
+  renderTab(tab) {
+    const { active } = this.state;
+    const isActive = active === tab;
+
+    return (
+      <TouchableOpacity
+        key={`tab-${tab}`}
+        onPress={() => this.handleTab(tab)}
+        style={[
+          styles.tab,
+          isActive ? styles.active : null
+        ]}
+      >
+        <Text size={16} medium gray={!isActive} secondary={isActive}>
+          {tab}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
-    console.log('state', this.state)
+    const { profile, navigation } = this.props;
+    const { categories } = this.state;
+    const tabs = ['Products', 'Inspirations', 'Shop'];
+
     return (
       <Block>
-        <Button gradient onPress={() => this.logOut()}>
-          <Text bold white center>Login</Text>
-        </Button>
-        <Button gradient onPress={() => this.props.navigation.navigate('MessageBoard')}>
-          <Text bold white center>Message Board</Text>
-        </Button>
+        <Block flex={false} row center space="between" style={styles.header}>
+          <Text h1 bold>Browse</Text>
+          <Button onPress={() => navigation.navigate('Settings')}>
+            <Image
+              source={profile.avatar}
+              style={styles.avatar}
+            />
+          </Button>
+        </Block>
+
+        <Block flex={false} row style={styles.tabs}>
+          {tabs.map(tab => this.renderTab(tab))}
+        </Block>
+
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{ paddingVertical: theme.sizes.base * 2}}
+        >
+          <Block flex={false} row space="between" style={styles.categories}>
+            {categories.map(category => (
+              <TouchableOpacity
+                key={category.name}
+                onPress={() => navigation.navigate('Explore', { category })}
+              >
+                <Card center middle shadow style={styles.category}>
+                  <Badge margin={[0, 0, 15]} size={50} color="rgba(41,216,143,0.20)">
+                    <Image source={category.image} />
+                  </Badge>
+                  <Text medium height={20}>{category.name}</Text>
+                  <Text gray caption>{category.count} products</Text>
+                </Card>
+              </TouchableOpacity>
+            ))}
+          </Block>
+        </ScrollView>
       </Block>
 
     )
   }
+}
+
+Browse.defaultProps = {
+  profile: mocks.profile,
+  categories: mocks.categories,
 }
 
 const mapStateToProps = (state) => {
@@ -78,3 +156,39 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Browse)
+
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: theme.sizes.base * 2,
+  },
+  avatar: {
+    height: theme.sizes.base * 2.2,
+    width: theme.sizes.base * 2.2,
+  },
+  tabs: {
+    borderBottomColor: theme.colors.gray2,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginVertical: theme.sizes.base,
+    marginHorizontal: theme.sizes.base * 2,
+  },
+  tab: {
+    marginRight: theme.sizes.base * 2,
+    paddingBottom: theme.sizes.base
+  },
+  active: {
+    borderBottomColor: theme.colors.secondary,
+    borderBottomWidth: 3,
+  },
+  categories: {
+    flexWrap: 'wrap',
+    paddingHorizontal: theme.sizes.base * 2,
+    marginBottom: theme.sizes.base * 3.5,
+  },
+  category: {
+    // this should be dynamic based on screen width
+    minWidth: (width - (theme.sizes.padding * 2.4) - theme.sizes.base) / 2,
+    maxWidth: (width - (theme.sizes.padding * 2.4) - theme.sizes.base) / 2,
+    maxHeight: (width - (theme.sizes.padding * 2.4) - theme.sizes.base) / 2,
+  }
+})
